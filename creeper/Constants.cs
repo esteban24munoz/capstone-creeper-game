7af404b9ace959d02ff1cb1b4e288e03e7ae09a8
Kreegaleeg <richardsonthomasandrew@gmail.com>
2026-01-26 18:52:03 -0600

    Added hover functionality and basic movement logic

---
+++ creeper/Constants.cs
@@ -0,0 +1,11 @@
+public static class Constants {
+    public const int GRID_X_DISTANCE = 57;
+    public const int GRID_Y_DISTANCE = 32;
+    public const int GRID_Y_OFFSET = 30;
+    public enum Player
+    {
+        Hero,
+        Enemy,
+        None
+    }
+}

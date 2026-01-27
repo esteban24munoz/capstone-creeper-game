9c01edd5f6e88fb981d8bd40577bb0a92dcd8a7a
Kreegaleeg <richardsonthomasandrew@gmail.com>
2026-01-26 22:32:51 -0600

    New textures

---
+++ creeper/Base Scripts/Base.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+public partial class Base : AnimatedSprite2D
+{
+    public override void _Ready()
+    {
+        Play("idle");
+    }
+}
--- creeper/Constants.cs
+++ creeper/Constants.cs
@@ -1,6 +1,6 @@
 public static class Constants {
-    public const int GRID_X_DISTANCE = 57;
-    public const int GRID_Y_DISTANCE = 32;
+    public const int GRID_X_DISTANCE = 48;
+    public const int GRID_Y_DISTANCE = 30;
     public const int GRID_Y_OFFSET = 30;
     public enum Player
     {

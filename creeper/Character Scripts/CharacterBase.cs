348673624e790fda1578b545c6f77e7d780106a9
Kreegaleeg <richardsonthomasandrew@gmail.com>
2026-01-29 19:13:10 -0600

    Complete character movement

--- creeper/Character Scripts/CharacterBase.cs
+++ creeper/Character Scripts/CharacterBase.cs
@@ -8,23 +8,49 @@ public partial class CharacterBase : AnimatedSprite2D
     [Signal] public delegate void MouseEnteredEventHandler(CharacterBase character);
     [Signal] public delegate void MouseExitedEventHandler(CharacterBase character);
 
+    private bool _isGhost = false;
+    public bool IsGhost
+    {
+        get {return _isGhost;}
+        set
+        {
+            if (value)
+            {
+            //Code to make the character transparent comes from Gemini
+            Color GhostColor = Modulate;
+            GhostColor.A = 0.6f;
+            SelfModulate = GhostColor;
+            _isGhost = value;
+            }
+            else
+            {
+            //Code to make the character transparent comes from Gemini
+            Color GhostColor = Modulate;
+            GhostColor.A = 1;
+            SelfModulate = GhostColor;
+            _isGhost = value;
+            }
+        }
+    }
+
     Area2D area;
     private int HoverOffset = 10;
-    private bool Hovering = false;
+    public bool Hovering = false;
+    private bool MouseOver = false;
     
     public override void _Ready()
     {
         Play("idle");
         area = GetNode<Area2D>("Area2D");
-        area.MouseEntered += () => EmitSignal(SignalName.MouseEntered, this);
-        area.MouseExited += () => EmitSignal(SignalName.MouseExited, this);
+        area.MouseEntered += () => {EmitSignal(SignalName.MouseEntered, this); MouseOver = true;};
+        area.MouseExited += () => {EmitSignal(SignalName.MouseExited, this); MouseOver = false;};
     }
 
     public void Hover()
     {
         if (Hovering) return;
         Position = new Vector2(Position.X, Position.Y - HoverOffset);
-        area.Position = new Vector2(area.Position.X, area.Position.Y + HoverOffset*2);
+        area.Position = new Vector2(area.Position.X, area.Position.Y + HoverOffset);
         Hovering = true;
     }
 
@@ -32,13 +58,13 @@ public partial class CharacterBase : AnimatedSprite2D
     {
         if (!Hovering) return;
         Position = new Vector2(Position.X, Position.Y + HoverOffset);
-        area.Position = new Vector2(area.Position.X, area.Position.Y - HoverOffset*2);
+        area.Position = new Vector2(area.Position.X, area.Position.Y - HoverOffset);
         Hovering = false;
     }
 
     public override void _Input(InputEvent @event)
     {
-        if (@event.IsActionPressed("mouse_click") && Hovering)
+        if (@event.IsActionPressed("mouse_click") && MouseOver)
         {
             EmitSignal(SignalName.OnClick, this);
         }

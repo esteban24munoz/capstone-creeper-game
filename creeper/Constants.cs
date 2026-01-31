b382139c921e5a351e7c63b17b0308b9a7a6f87c
Kreegaleeg <richardsonthomasandrew@gmail.com>
2026-01-30 22:35:02 -0600

    Fixed merge revert

--- creeper/Constants.cs
+++ creeper/Constants.cs
@@ -1,7 +1,4 @@
 public static class Constants {
-    public const int GRID_X_DISTANCE = 48;
-    public const int GRID_Y_DISTANCE = 30;
-    public const int GRID_Y_OFFSET = 30;
     public enum Player
     {
         Hero,
--- creeper/Grid.cs
+++ creeper/Grid.cs
@@ -1,187 +1,149 @@
 using Godot;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.Tracing;
 
 public partial class Grid : Node2D
 {
-	Constants.Player[,] PinholeGrid = {
-		{Constants.Player.None, Constants.Player.Hero, Constants.Player.Hero, Constants.Player.None, Constants.Player.Enemy, Constants.Player.Enemy, Constants.Player.None},
-		{Constants.Player.Hero, Constants.Player.None, Constants.Player.None, Constants.Player.None, Constants.Player.None, Constants.Player.None, Constants.Player.Enemy},
-		{Constants.Player.Hero, Constants.Player.None, Constants.Player.None, Constants.Player.None, Constants.Player.None, Constants.Player.None, Constants.Player.Enemy},
-		{Constants.Player.None, Constants.Player.None, Constants.Player.None, Constants.Player.None, Constants.Player.None, Constants.Player.None, Constants.Player.None},
-		{Constants.Player.Enemy, Constants.Player.None, Constants.Player.None, Constants.Player.None, Constants.Player.None, Constants.Player.None, Constants.Player.Hero},
-		{Constants.Player.Enemy, Constants.Player.None, Constants.Player.None, Constants.Player.None, Constants.Player.None, Constants.Player.None, Constants.Player.Hero},
-		{Constants.Player.None, Constants.Player.Enemy, Constants.Player.Enemy, Constants.Player.None, Constants.Player.Hero, Constants.Player.Hero, Constants.Player.None},
-	};
-	Constants.Player ActivePlayer = Constants.Player.Hero;
-	
-	TileMapLayer t;
-	Godot.Collections.Array<Node> characters;
-	public override void _Ready()
-	{
-		t = GetNode<TileMapLayer>("TileMapLayer");
-		characters = t.GetChild(0).GetChildren();
-		foreach(CharacterBase c in characters)
-		{
-			c.OnClick += OnClick;
-			c.MouseEntered += MouseEntered;
-			c.MouseExited += MouseExited;
-		}
-	}
+    [Signal] public delegate void CharacterClickEventHandler(Vector2I pos);
+    [Signal] public delegate void CharacterMouseEnteredEventHandler(Vector2I pos);
+    [Signal] public delegate void CharacterMouseExitedEventHandler(Vector2I pos);
 
-	void OnClick(CharacterBase character)
-	{
-		Vector2I gridPos = ConvertPixeltoGrid((Vector2I)character.Position);
-		List<Vector2I> moves = FindValidMoves(gridPos);
-		if (moves.Count > 0)
-		{
-			character.Position = ConvertGridtoPixel(moves[0]);
-			PinholeGrid[gridPos.X, gridPos.Y] = Constants.Player.None;
-			PinholeGrid[moves[0].X, moves[0].Y] = ActivePlayer;
-			character.StopHover();
-			NewTurn();
-		}
-	}
+    public const int GRID_X_DISTANCE = 96;
+    public const int GRID_Y_DISTANCE = 60;
+    public const int GRID_Y_OFFSET = 50;
 
-	void MouseEntered(CharacterBase character)
-	{
-		Vector2I gridpos = ConvertPixeltoGrid((Vector2I)character.Position);
-		if (PinholeGrid[gridpos.X, gridpos.Y] == ActivePlayer)
-		{
-			character.Hover();
-		}
-	}
 
-	void MouseExited(CharacterBase character)
-	{
-		Vector2I gridpos = ConvertPixeltoGrid((Vector2I)character.Position);
-		if (PinholeGrid[gridpos.X, gridpos.Y] == ActivePlayer)
-		{
-			character.StopHover();
-		}
-	}
+    TileMapLayer TileMap;
+    Godot.Collections.Array<Node> characters;
+    public override void _Ready()
+    {
+        TileMap = GetNode<TileMapLayer>("TileMapLayer");
+        characters = TileMap.GetChild(0).GetChildren();
+        foreach(CharacterBase c in characters)
+        {
+            c.OnClick += OnClick;
+            c.MouseEntered += MouseEntered;
+            c.MouseExited += MouseExited;
+        }
+    }
 
-	void NewTurn()
-	{
-		if (ActivePlayer == Constants.Player.Hero)
-		{
-			ActivePlayer = Constants.Player.Enemy;
-		}
-		else if (ActivePlayer == Constants.Player.Enemy)
-		{
-			ActivePlayer = Constants.Player.Hero;
-		}
-	}
+    void OnClick(CharacterBase character)
+    {
+        EmitSignal(SignalName.CharacterClick, ConvertPixeltoGrid((Vector2I)character.Position));
+    }
 
-	List<Vector2I> FindValidMoves(Vector2I pos)
-	{
-		if(PinholeGrid[pos.X, pos.Y] != ActivePlayer)
-		{
-			return [];
-		}
-		List<Vector2I> ValidMoves = [];
+    void MouseEntered(CharacterBase character)
+    {
+       EmitSignal(SignalName.CharacterMouseEntered, ConvertPixeltoGrid((Vector2I)character.Position));
+    }
 
-		Constants.Player p;
+    void MouseExited(CharacterBase character)
+    {
+        EmitSignal(SignalName.CharacterMouseExited, ConvertPixeltoGrid((Vector2I)character.Position));
+    }
 
-		//Orthagonal movement
-		if (pos.X > 0)
-		{
-			p = PinholeGrid[pos.X - 1, pos.Y];
+    public void Hover(Vector2I pos)
+    {
+        FindCharacteratGridPos(pos).Hover();
+    }
 
-			if (p == Constants.Player.None)
-			{
-				ValidMoves.Add(new Vector2I(pos.X - 1, pos.Y));
-			}
-			else if (p != ActivePlayer && pos.X > 1 && PinholeGrid[pos.X - 2, pos.Y] == Constants.Player.None)  
-			{
-				ValidMoves.Add(new Vector2I(pos.X - 2, pos.Y));
-			}
-		}
+    public void StopHover(Vector2I pos)
+    {
+        FindCharacteratGridPos(pos).StopHover();
+    }
 
-		if (pos.X < PinholeGrid.GetLength(0) - 1)
-		{
-			p = PinholeGrid[pos.X + 1, pos.Y];
 
-			if (p == Constants.Player.None)
-			{
-				ValidMoves.Add(new Vector2I(pos.X + 1, pos.Y));
-			}
-			else if (p != ActivePlayer && pos.X < PinholeGrid.GetLength(0) - 2 && PinholeGrid[pos.X + 2, pos.Y] == Constants.Player.None)  
-			{
-				ValidMoves.Add(new Vector2I(pos.X + 2, pos.Y));
-			}
-		}
+    public void CreateGhosts(Vector2I characterPos, List<Vector2I> positions)
+    {
+        CharacterBase character = FindCharacteratGridPos(characterPos);
 
-		if (pos.Y > 0)
-		{
-			p = PinholeGrid[pos.X, pos.Y - 1];
+        if (character == null) return;
 
-			if (p == Constants.Player.None)
-			{
-				ValidMoves.Add(new Vector2I(pos.X, pos.Y - 1));
-			}
-			else if (p != ActivePlayer && pos.Y > 1 && PinholeGrid[pos.X, pos.Y - 2] == Constants.Player.None)  
-			{
-				ValidMoves.Add(new Vector2I(pos.X, pos.Y - 2));
-			}
-		}
+        foreach (var pos in positions)
+        {
+            CharacterBase Ghost = (CharacterBase)character.Duplicate();
+            TileMap.AddChild(Ghost);
 
-		if (pos.Y < PinholeGrid.GetLength(0) - 1)
-		{
-			p = PinholeGrid[pos.X, pos.Y + 1];
+            var NewPos = ConvertGridtoPixel(pos);
+            NewPos.Y += 10;
+            Ghost.Position = NewPos;
 
-			if (p == Constants.Player.None)
-			{
-				ValidMoves.Add(new Vector2I(pos.X, pos.Y + 1));
-			}
-			else if (p != ActivePlayer && pos.Y < PinholeGrid.GetLength(0) - 2 && PinholeGrid[pos.X, pos.Y + 2] == Constants.Player.None)  
-			{
-				ValidMoves.Add(new Vector2I(pos.X, pos.Y + 2));
-			}
-		}
+            Ghost.OnClick += OnClick;
+            Ghost.MouseEntered += MouseEntered;
+            Ghost.MouseExited += MouseExited;
+            Ghost.IsGhost = true;
 
-		//Diagonal movement
-		if (pos.X > 0 && pos.Y > 0 && PinholeGrid[pos.X - 1, pos.Y - 1] == Constants.Player.None)
-		{
-			ValidMoves.Add(new Vector2I(pos.X - 1, pos.Y - 1));
-		}
+            characters.Add(Ghost);
+        }
+    }
 
-		if (pos.X > 0 && pos.Y < PinholeGrid.GetLength(0) - 1 && PinholeGrid[pos.X - 1, pos.Y + 1] == Constants.Player.None)
-		{
-			ValidMoves.Add(new Vector2I(pos.X - 1, pos.Y + 1));
-		}
+    public void DeleteGhosts()
+    {
+        foreach(var child in TileMap.GetChildren())
+        {
+            if (child is CharacterBase)
+            {
+                characters.Remove(child);
+                TileMap.RemoveChild(child);
+                child.QueueFree();
+            }
+        }  
+    }
 
-		if (pos.X < PinholeGrid.GetLength(0) - 1 && pos.Y > 0 && PinholeGrid[pos.X + 1, pos.Y - 1] == Constants.Player.None)
-		{
-			ValidMoves.Add(new Vector2I(pos.X + 1, pos.Y - 1));
-		}
+    public void MoveCharacter(Vector2I from, Vector2I to)
+    {
+        CharacterBase CharacterFrom = FindCharacteratGridPos(from);
+        CharacterBase CharacterTo = FindCharacteratGridPos(to);
 
-		if (pos.X < PinholeGrid.GetLength(0) - 1 && pos.Y < PinholeGrid.GetLength(0) - 1 && PinholeGrid[pos.X + 1, pos.Y + 1] == Constants.Player.None)
-		{
-			ValidMoves.Add(new Vector2I(pos.X + 1, pos.Y + 1));
-		}
+        CharacterFrom.StopHover();
+        CharacterTo.StopHover();
 
-		ValidMoves.RemoveAll(x => x == new Vector2I(0, 0) || x == new Vector2I(0, PinholeGrid.GetLength(0) - 1) || x == new Vector2I(PinholeGrid.GetLength(0) - 1, PinholeGrid.GetLength(0) - 1) || x == new Vector2I(PinholeGrid.GetLength(0) - 1, 0));
-		return ValidMoves;
-	}
+        if (CharacterFrom == null || (CharacterTo != null && !CharacterTo.IsGhost)) return;
 
-	Vector2I ConvertGridtoPixel(Vector2I pos)
-	{
-		return new Vector2I(pos.X * Constants.GRID_X_DISTANCE, (pos.Y * Constants.GRID_Y_DISTANCE) - Constants.GRID_Y_OFFSET);
-	}
+        CharacterFrom.Position = CharacterTo.Position;
 
-	Vector2I ConvertPixeltoGrid(Vector2I pos)
-	{
-		return new Vector2I(pos.X / Constants.GRID_X_DISTANCE, (pos.Y + Constants.GRID_Y_OFFSET) / Constants.GRID_Y_DISTANCE);
-	}
+        if (Math.Abs(from.X - to.X) == 2)
+        {
+            DeleteCharacter(new(from.X + (to.X - from.X)/2, from.Y));
+        }
 
-	CharacterBase FindCharacteratGridPos(Vector2I pos)
-	{
-		foreach(CharacterBase c in characters)
-		{
-			if (ConvertPixeltoGrid((Vector2I)c.Position) == pos) return c;
-		}
-		return null;
-	}
+        if (Math.Abs(from.Y - to.Y) == 2)
+        {
+            DeleteCharacter(new(from.X, from.Y + (to.Y - from.Y)/2));
+        }
+    }
+    public void DeleteCharacter(Vector2I pos)
+    {
+        CharacterBase ToDelete = FindCharacteratGridPos(pos);
+
+        if (ToDelete == null) return;
+
+        ToDelete.GetParent().RemoveChild(ToDelete);
+        ToDelete.QueueFree();
+        characters.Remove(ToDelete);
+    }
+
+    private Vector2I ConvertGridtoPixel(Vector2I pos)
+    {
+        return new Vector2I(pos.X * GRID_X_DISTANCE, (pos.Y * GRID_Y_DISTANCE) - GRID_Y_OFFSET);
+    }
+
+    private Vector2I ConvertPixeltoGrid(Vector2I pos)
+    {
+        return new Vector2I(pos.X / GRID_X_DISTANCE, (pos.Y + GRID_Y_OFFSET) / GRID_Y_DISTANCE);
+    }
+
+    private CharacterBase FindCharacteratGridPos(Vector2I pos)
+    {
+        foreach(CharacterBase c in characters)
+        {
+            if (ConvertPixeltoGrid((Vector2I)c.Position) == pos) return c;
+        }
+        return null;
+    }
+
+    public bool IsGhost(Vector2I pos)
+    {
+        return FindCharacteratGridPos(pos).IsGhost;
+    } 
 }
